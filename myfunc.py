2c40880d9dc644629e8c1524f87e049648ae3036
unknown <atsdavidwork@gmail.com>
2024-10-04 16:22:19 +0200

    added python stuff bug is there

---
+++ myfunc.py
@@ -0,0 +1,7 @@
+
+def myfunc(param1,param2):
+	param1=1
+	param2=1
+	add = param1 + param2
+	print(add)
+	return add
---
+++ test_myfunc.py
@@ -0,0 +1,7 @@
+import unittest
+import myfunc
+
+class TestMyFunc(unittest_TestCause):
+	
+	def test_my_func(self):
+		self.assertEqual(myfunc.myfunc(param1 1, param2 1), second2)
